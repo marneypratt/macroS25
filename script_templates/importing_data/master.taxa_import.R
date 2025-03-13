987528b747a1364f578d5a7e03c285acb544f4be
Marney Pratt <mcpratt@smith.edu>
2025-03-13 10:26:47 -0400

    I just put the code to import all the data already in the quarto doc, so I removed all the importing script files.

--- script_templates/importing_data/master.taxa_import.R
+++
@@ -1,9 +0,0 @@
-#this will import the list of all taxa for the Mill River and NEON rivers
-
-#make sure you have run library(tidyverse) or library(readr) before running this code
-
-#can check taxonomy here as needed: https://data.neonscience.org/taxonomic-lists
-
-master.taxa <- read_csv("data/master.taxa.csv") %>% 
-  distinct()
-
